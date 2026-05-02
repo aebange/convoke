9d84920e8f0600ff5b4eacf56544636110c000ab
Aeban <aebange@outlook.com>
2026-05-02 15:44:19 -0400

    CON-5 / Refactored AA16 template for new project / Removed vestigial components, tables, and services from the project
    Signed-off-by: Aeban <aebange@outlook.com>

---
+++ backend/src/main/java/com/convoke/Application.java
@@ -0,0 +1,40 @@
+package com.convoke;
+
+import com.convoke.services.VersionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * Main Application
+ **/
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class Application
+{
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+
+    /**
+     * Web Application Starts Here
+     **/
+    public static void main( String[] args )
+    {
+        logger.debug("main() started.");
+
+        // Start up Spring Boot but disable the banner
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        ConfigurableApplicationContext context = app.run(args);
+
+        // Get a reference to the versionService
+        VersionService versionService = (VersionService) context.getBean("com.lessons.services.VersionService");
+
+        // Log a message so that Tier 3 Support knows which version and commit Id is actually running
+        logger.info("WebApp is Up / {} / {}", versionService.getAppVersion(), versionService.getCommitId() );
+    }
+}
